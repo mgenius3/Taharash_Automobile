9448a8a8d288438dda4747d7a159bb77f3937224
mgenius3 <benmos16@gmail.com>
2023-09-23 13:13:12 +0000

    update some text

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require("express");
-const next = require("next");
-const dotenv = require("dotenv");
-const dev = process.env.NODE_ENV !== "production";
+const path = require("path");
 
-dotenv.config();
-const app = next({ dev });
-const handle = app.getRequestHandler();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const errorHandler = require("./middleware/errorHandler");
 const morgan = require("morgan");
 const { createTableUser } = require("./database/auth");
@@ -14,56 +9,65 @@ const { createTableBlog } = require("./database/blog");
 const { createTableAutomobile } = require("./database/automobile");
 const { createTableContact } = require("./database/contact");
 
-app.prepare().then(() => {
-  const server = express();
+// app.prepare().then(() => {
+const server = express();
 
-  createTableUser();
-  createTableBlog();
-  createTableAutomobile();
-  createTableContact();
+createTableUser();
+createTableBlog();
+createTableAutomobile();
+createTableContact();
 
-  server.use(morgan("dev"));
+server.use(morgan("dev"));
+// Middleware to parse JSON request body
+// server.use(bodyParser.json());
+// Default route handler
+server.use(express.static(path.join(__dirname, "out")));
+server.use("/styles", express.static(path.join(__dirname, "/styles")));
+server.use(express.json({ limit: "10mb" }));
+server.use("/serve/blog", require("./routes/blog"));
+server.use("/serve/automobile", require("./routes/automobile"));
+server.use("/serve/user", require("./routes/auth"));
+server.use("/serve/admin", require("./routes/admin"));
 
-  // Middleware to parse JSON request body
-  server.use(express.json({ limit: "10mb" }));
-  server.use("/serve/blog", require("./routes/blog"));
-  server.use("/serve/automobile", require("./routes/automobile"));
-  server.use("/serve/user", require("./routes/auth"));
-  server.use("/serve/admin", require("./routes/admin"));
-
-  // error handling
-  server.use(errorHandler);
-
-  // Default route handler
-  server.get("*", (req, res) => {
-    return handle(req, res);
+server.get("/sitemap.xml", (req, res) => {
+  const filePath = path.join(__dirname, "sitemap.xml");
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.status(404).end();
+    } else {
+      res.setHeader("Content-Type", "application/xml");
+      res.send(data);
+    }
   });
+});
 
-  // Middleware to handle requests that don't match any route
-  server.use(function (req, res) {
-    res.status(404);
+// error handling
+server.use(errorHandler);
 
-    // Render a custom error page
-    res.json({
-      title: "Page Not Found",
-      message: "The requested page does not exist",
-    });
+// Middleware to handle requests that don't match any route
+server.use(function (req, res) {
+  res.status(404);
+
+  // Render a custom error page
+  res.json({
+    title: "Page Not Found",
+    message: "The requested page does not exist",
   });
+});
 
-  // Error handler middleware
-  server.use(function (err, req, res) {
-    console.error(err.stack);
-    res.status(500);
+// Error handler middleware
+server.use(function (err, req, res) {
+  console.error(err.stack);
+  res.status(500);
 
-    // render a custom error page
-    res.json({
-      title: "Server Error",
-      message: "Something went wrong on our end",
-    });
+  // render a custom error page
+  res.json({
+    title: "Server Error",
+    message: "Something went wrong on our end",
   });
+});
 
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${port}`);
 });
